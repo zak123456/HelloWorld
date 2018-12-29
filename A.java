d6473d3c7ac1146c0baa7d09e82728a81a947e7e
zakir <mohammedzakirhusain@gmail.com>
2018-12-29 12:02:26 +0530

    modified java file

--- A.java
+++ A.java
@@ -1,6 +1,6 @@
 public class A {
     public static void main(String args[]){  
        System.out.println("Hello World DevOps");
-	System.ou.println("Hello World second line");
+	System.out.println("Hello World second line");
     }
 }
